ab43aa7919f330b3a08ea5f619f3742eef892ae4
Akash Singh <88923478+AkashSingh2002@users.noreply.github.com>
2024-12-06 08:53:47 +0000

    commit

--- src/Components/GridCard/index.js
+++ src/Components/GridCard/index.js
@@ -1,20 +1,24 @@
 import React from "react";
-import './index.css'
+import './index.css';
 
-const GridCard = ({product}) => {
-  var image = require('../../Assets/Images/'+product.img)
+const GridCard = ({ product, isFavorite, toggleFavorite }) => {
+  const image = require('../../Assets/Images/' + product.img);
+
   return (
-    <div className='col-4 col-sm-3 mx-3 my-2'>
-      <div className="row d-flex justify-content-center featured">
-        <img src={image} alt={product.name} className="img-fluid featured-img" />
-      </div>
-      <br />
-      <div className="text-center featured-head">
-      {product.name}
-      </div>
-      <div className="text-center featured-para">
-      {product.nprice} &nbsp;
-      <span className="latest-mini">{product.oprice}</span>
+    <div className="col-4 col-sm-3 mx-3 my-2">
+      <div className="featured-card">
+        <div className="favorite-icon" onClick={toggleFavorite}>
+          <i className={`fa ${isFavorite ? 'fa-heart' : 'fa-heart-o'}`} aria-hidden="true"></i>
+        </div>
+        <div className="row d-flex justify-content-center featured">
+          <img src={image} alt={product.name} className="img-fluid featured-img" />
+        </div>
+        <br />
+        <div className="text-center featured-head">{product.name}</div>
+        <div className="text-center featured-para">
+          {product.nprice} &nbsp;
+          <span className="latest-mini">{product.oprice}</span>
+        </div>
       </div>
     </div>
   );
--- src/Pages/ShopGrid/index.js
+++ src/Pages/ShopGrid/index.js
@@ -8,14 +8,25 @@ import { GetProducts } from "../../Layouts/Main";
 import './index.css';
 
 function ShopGrid() {
-  
-    const [view, setView] = useState('grid');
-  
-    
+  const [view, setView] = useState('grid');
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [favorites, setFavorites] = useState(new Set()); // Tracks favorite product IDs
+
+  const toggleFavorite = (id) => {
+    setFavorites((prev) => {
+      const updated = new Set(prev);
+      if (updated.has(id)) {
+        updated.delete(id);
+      } else {
+        updated.add(id);
+      }
+      return updated;
+    });
+  };
 
   return (
     <>
-      <Jumbotron style={{ "background-color": "#F6F5FF" }}>
+      <Jumbotron style={{ backgroundColor: "#F6F5FF" }}>
         <Container>
           <Row>
             <Col>
@@ -26,100 +37,133 @@ function ShopGrid() {
       </Jumbotron>
 
       <Container>
-      <Row>
-        <Col className="col-12 col-md-5 col-xl-6">
-          <h3 className="grid-head">Ecommerce Accesories & Fashion item</h3>
-          <h5 className="grid-head">About 9,620 results (0.62 seconds)</h5>
-        </Col>
-
-        <Col className="col-12 col-md-7 col-xl-6">
         <Row>
-        <Col className="col-4">
-            <Row>
-                <Col className="col-12 col-lg-6">
-          <label for="cars"> <h5 className="grid-head">Sort -:</h5></label>
-                </Col>
-                <Col className="col-12 col-lg-6 selectGrid align-item-center d-flex">
-        <select id="cars" name="cars" >
-            <option value="Match">Match</option>
-            <option value="Match">Match</option>
-            <option value="Match">Match</option>
-            <option value="Match">Match</option>
-          </select>
-                </Col>
-            </Row>
-
+          <Col className="col-12 col-md-5 col-xl-6">
+            <h3 className="grid-head">Ecommerce Accessories & Fashion items</h3>
+            <h5 className="grid-head">About 9,620 results (0.62 seconds)</h5>
           </Col>
-       
-        <Col className="col-4">
-        <Row>
-            <Col className="col-12 col-lg-4">
-          <h5 className="grid-head">View-:</h5>
-            </Col>
-            <Col className="col-12 col-lg-8">
-        <Button onClick={() => {setView('grid')}} className="btn-grid">
-        <i class="fa fa-lg fa-table"></i>
-        </Button>
-        <Button onClick={() => {setView('list')}} className="btn-grid">
-        <i class="fa fa-lg fa-list"></i>
-        </Button>
-            </Col>
-        </Row>
-        </Col>
-        <Col className="col-4">
-          <div class="wrap">
-            <div class="search">
-              <input type="text" class="searchTerm" placeholder="Search" />
-              <button type="submit" class="searchButton">
-                <i class="fa fa-search"></i>
-              </button>
-            </div>
-          </div>
-        </Col>
-        </Row>
-        </Col>
-      </Row>
-    </Container>
-    
-   { view === 'grid' && <ShopGridDefault /> }
-   { view === 'list' && <ShopList /> }
 
-      
+          <Col className="col-12 col-md-7 col-xl-6">
+            <Row>
+            <Col className="col-4 d-flex align-items-center">
+  <Row className="w-100">
+    <Col className="col-6 d-flex align-items-center">
+      <label htmlFor="sort">
+        <h5 className="grid-head">Sort:</h5>
+      </label>
+    </Col>
+    <Col className="col-6">
+      <select
+        id="sort"
+        name="sort"
+        className="form-select"
+        style={{
+          width: "100%", // Ensures it fits well within its column
+          maxWidth: "150px", // Limits dropdown width
+          marginLeft: "8px", // Adds spacing between label and dropdown
+        }}
+      >
+        <option value="Match">Match</option>
+        <option value="Newest">Newest</option>
+        <option value="PriceLowHigh">Price: Low to High</option>
+        <option value="PriceHighLow">Price: High to Low</option>
+      </select>
+    </Col>
+  </Row>
+</Col>
+
+
+              <Col className="col-4">
+                <Row>
+                  <Col className="col-12 col-lg-4">
+                    <h5 className="grid-head">View-:</h5>
+                  </Col>
+                  <Col className="col-12 col-lg-8">
+                    <Button onClick={() => setView('grid')} className="btn-grid">
+                      <i className="fa fa-lg fa-table"></i>
+                    </Button>
+                    <Button onClick={() => setView('list')} className="btn-grid">
+                      <i className="fa fa-lg fa-list"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </Col>
+
+              <Col className="col-4">
+                <div className="wrap">
+                  <div className="search">
+                    <input type="text" className="searchTerm" placeholder="Search" />
+                    <button type="submit" className="searchButton">
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className="d-flex justify-content-end">
+            <Button
+              className="btn-grid"
+              onClick={() => setShowFavorites((prev) => !prev)}
+            >
+              {showFavorites ? "Show All" : "Show Favorites"}
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+
+      {view === 'grid' && (
+        <ShopGridDefault
+          showFavorites={showFavorites}
+          favorites={favorites}
+          toggleFavorite={toggleFavorite}
+        />
+      )}
+      {view === 'list' && <ShopList />}
     </>
   );
 }
 
-function ShopGridDefault(){
-
+function ShopGridDefault({ showFavorites, favorites, toggleFavorite }) {
   const products = useContext(GetProducts);
+
+  const filteredProducts = showFavorites
+    ? products.shopGrid?.filter((product) => favorites.has(product.id))
+    : products.shopGrid;
+
   return (
     <div className="row justify-content-center d-flex">
-        <div className="row container d-flex justify-content-center my-5">
-          {products.shopGrid &&
-            products.shopGrid
-              .slice(0, 12)
-              .map((product) => (
-                <GridCard product={product} key={product.id} />
-              ))}
-        </div>
+      <div className="row container d-flex justify-content-center my-5">
+        {filteredProducts &&
+          filteredProducts.slice(0, 12).map((product) => (
+            <GridCard
+              product={product}
+              key={product.id}
+              isFavorite={favorites.has(product.id)}
+              toggleFavorite={() => toggleFavorite(product.id)}
+            />
+          ))}
       </div>
-  )
+    </div>
+  );
 }
 
-function ShopList(){
+function ShopList() {
   const products = useContext(GetProducts);
+
   return (
     <div className="row justify-content-center d-flex">
-        <div className="row container d-flex justify-content-center my-5">
-          {products.listView &&
-            products.listView
-              .slice(0, 7)
-              .map((product) => (
-                <ListCard product={product} key={product.id} />
-              ))}
-        </div>
+      <div className="row container d-flex justify-content-center my-5">
+        {products.listView &&
+          products.listView.slice(0, 7).map((product) => (
+            <ListCard product={product} key={product.id} />
+          ))}
       </div>
-  )
+    </div>
+  );
 }
 
 export default ShopGrid;
